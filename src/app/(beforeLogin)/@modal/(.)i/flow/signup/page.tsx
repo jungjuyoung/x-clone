6ed3d6a4900164edcf80b04eed89a5caecc947f7
jungjuyoung <nwwn1004@naver.com>
2023-12-21 17:20:24 +0900

    feat: intercepting route

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupModal from "@/app/(beforeLogin)/_component/SignupModal";
+
+export default function page() {
+  return (
+    <>
+      가로채기 라우트!!!!
+      <SignupModal />;
+    </>
+  );
+}
