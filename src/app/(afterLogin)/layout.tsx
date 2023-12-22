3c03c98b95b7a78f988c5d793a8d7110f7a65dfd
jungjuyoung <nwwn1004@naver.com>
2023-12-23 00:40:56 +0900

    feat: layout

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,12 +1,33 @@
+import style from "@/app/(afterLogin)/layout.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import zlogo from "../../../public/zlogo.png";
 export default function AfterLoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      (afterLogin) layout
-      <div>{children}</div>
+    <div className={style.container}>
+      <header className={style.leftSectionWrapper}>
+        <section className={style.leftSection}>
+          <div className={style.leftSectionFixed}>
+            <Link className={style.logo} href="/home">
+              <div className={style.logoPill}>
+                <Image src={zlogo} alt="z.com로고" width={40} height={40} />
+              </div>
+            </Link>
+          </div>
+        </section>
+      </header>
+      <div className={style.rightSectionWrapper}>
+        <div className={style.rightSectionInner}>
+          <main className={style.main}>{children}</main>
+          <section className={style.rightSection}>
+            <form className={style.search}></form>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
