8b01b4c348cf7cc112501a35e18d097ef8076b70
jungjuyoung <nwwn1004@naver.com>
2023-12-26 01:20:25 +0900

    feat: Home Tab

---
+++ src/app/(afterLogin)/home/_component/Tab.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useState } from "react";
+import style from "./tab.module.css";
+
+export default function Tab() {
+  const [tab, setTab] = useState("recommend");
+  const onClickRecommend = () => {
+    setTab("recommend");
+  };
+  const onClickFollowing = () => {
+    setTab("following");
+  };
+  return (
+    <div className={style.homeFixed}>
+      <div className={style.homeText}>홈</div>
+      <div className={style.homeTab}>
+        <div
+          className={`${tab === "recommend" ? style.on : ""}`}
+          onClick={onClickRecommend}
+        >
+          추천
+        </div>
+        <div
+          className={`${tab === "following" ? style.on : ""}`}
+          onClick={onClickFollowing}
+        >
+          팔로우중
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,18 @@
+import Post from "../_component/Post";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 };
 
 export default Home;
