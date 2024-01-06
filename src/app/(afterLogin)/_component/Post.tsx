9b9b7fb69acdb980a9662626a5964433fb7dc3d8
jungjuyoung <nwwn1004@naver.com>
2024-01-07 01:13:22 +0900

    feat: PostImages component

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,7 +6,7 @@ import "dayjs/locale/ko";
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
 import { faker } from "@faker-js/faker";
-// import PostImages from "@/app/(afterLogin)/_component/PostImages";
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
@@ -59,15 +59,7 @@ export default function Post({ noImage }: Props) {
           </div>
           <div>{target.content}</div>
           <div>
-            {/* <PostImages post={target} /> */}
-            {target.Images && target.Images.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0]?.imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt="" />
-              </Link>
-            )}
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
