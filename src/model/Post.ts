ff51cbc6e925642ba4a1b3da744e1181da68043c
jungjuyoung <nwwn1004@naver.com>
2024-01-19 01:29:09 +0900

    feat: extract commonly use Types

---
+++ src/model/Post.ts
@@ -0,0 +1,11 @@
+import { PostImage } from './PostImage';
+import { User } from './User';
+
+export interface Post {
+  noImage?: boolean;
+  postId: number;
+  content: string;
+  createdAt: Date;
+  User: User;
+  Images: PostImage[]
+}
