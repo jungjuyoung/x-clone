d66320cb42901859297db1ec5bd4b8dc74adfa20
jungjuyoung <nwwn1004@naver.com>
2024-01-22 02:41:42 +0900

    feat: react-query option

---
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getPostRecommends } from "@/app/(afterLogin)/home/_lib/getPostRecommends";
+import Post from "@/app/(afterLogin)/_component/Post";
+import { Post as IPost } from "@/model/Post";
+
+export default function PostRecommends() {
+  const { data } = useQuery<IPost[]>({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+    staleTime: 60 * 1000, // fresh -> stale, 기본값이 0. 5분이라는 기준
+    gcTime: 300 * 1000, // 기본 5분 설정
+  });
+
+  console.log("PostRecommends data: ", data);
+
+  return (
+    <>
+      {data?.map((post) => (
+        <Post key={post.postId} post={post} />
+      ))}
+    </>
+  );
+}
