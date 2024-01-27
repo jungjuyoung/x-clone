1f56ac0b32453d8957d0219aeb398c09b89ecbcd
jungjuyoung <nwwn1004@naver.com>
2024-01-28 03:44:23 +0900

    feat: posts

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,28 +1,45 @@
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import style from "./singlePost.module.css";
-import Post from "@/app/(afterLogin)/_component/Post";
-import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+import Comments from "../[id]/_component/Comments";
+import CommentForm from "../[id]/_component/CommentForm";
+import SinglePost from "../[id]/_component/SinglePost";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getSinglePost } from "./_lib/getSinglePost";
+import { getComments } from "./_lib/getComments";
 
-export default function SinglePost() {
+type Props = {
+  params: { id: string };
+};
+export default async function Page({ params }: Props) {
+  const { id } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", id],
+    queryFn: getSinglePost,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", id, "comments"],
+    queryFn: getComments,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <h3 className={style.headerTitle}>게시하기</h3>
-      </div>
-      <Post />
-      <CommentForm />
-      <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      <HydrationBoundary state={dehydratedState}>
+        <div className={style.header}>
+          <BackButton />
+          <h3 className={style.headerTitle}>게시하기</h3>
+        </div>
+        <SinglePost id={id} />
+        <CommentForm />
+        <div>
+          <Comments id={id} />
+        </div>
+      </HydrationBoundary>
     </div>
   );
 }
