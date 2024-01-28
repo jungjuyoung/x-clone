0c3f65e17f263333a6333c9009cb94061e0ac712
jungjuyoung <nwwn1004@naver.com>
2024-01-29 01:33:45 +0900

    feat: try using the "Trend" key as getQueryData

--- src/app/(afterLogin)/explore/_component/TrendSection.tsx
+++ src/app/(afterLogin)/explore/_component/TrendSection.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import Trend from "@/app/(afterLogin)/_component/Trend";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { HashTag } from "@/model/HashTag";
 import { getTrends } from "@/app/(afterLogin)/_lib/getTrends";
 
 export default function TrendSection() {
-  const { data } = useQuery<HashTag []>({
-    queryKey: ["trends"],
-    queryFn: getTrends,
-    staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
-    gcTime: 300 * 1000,
-  });
-  return data?.map((trend) => <Trend trend={trend} key={trend.tagId} />);
+  const queryClient = useQueryClient();
+  const data = queryClient.getQueryData(["trends"]);
+  // const { data } = useQuery<HashTag[]>({
+  //   queryKey: ["trends"],
+  //   queryFn: getTrends,
+  //   staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+  //   gcTime: 300 * 1000,
+  // });
+  return data?.map((trend: HashTag) => (
+    <Trend trend={trend} key={trend.tagId} />
+  ));
 }
