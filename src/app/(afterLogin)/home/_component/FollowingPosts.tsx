cee65133e92c55a67d17335bc045c09654e309fd
jungjuyoung <nwwn1004@naver.com>
2024-01-31 17:23:15 +0900

    feat: followingPost infinite scroll

--- src/app/(afterLogin)/home/_component/FollowingPosts.tsx
+++ src/app/(afterLogin)/home/_component/FollowingPosts.tsx
@@ -1,18 +1,40 @@
 "use client";
-
-import { useQuery } from "@tanstack/react-query";
+import { Fragment, useEffect } from "react";
+import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
 import { getFollowingPosts } from "@/app/(afterLogin)/home/_lib/getFollowingPosts";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { Post as IPost } from "@/model/Post";
 import styles from "@/app/(afterLogin)/home/home.module.css";
+import { useInView } from "react-intersection-observer";
+import { NextPage } from "next";
 
 export default function FollowingPosts() {
-  const { data, isPending } = useQuery<IPost[]>({
-    queryKey: ["posts", "followings"],
-    queryFn: getFollowingPosts,
-    staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
-    gcTime: 300 * 1000,
+  const { data, isPending, isFetching, fetchNextPage, hasNextPage } =
+    useInfiniteQuery<
+      IPost[],
+      Object,
+      InfiniteData<IPost[]>,
+      [_1: string, _2: string],
+      number
+    >({
+      queryKey: ["posts", "followings"],
+      queryFn: getFollowingPosts,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
+      staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+      gcTime: 300 * 1000,
+    });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0,
   });
+
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
 
   if (isPending) {
     return (
@@ -48,5 +70,16 @@ export default function FollowingPosts() {
     );
   }
 
-  return data?.map((post) => <Post key={post.postId} post={post} />);
+  return (
+    <>
+      {data?.pages.map((page, i) => (
+        <Fragment key={i}>
+          {page?.map((post) => (
+            <Post key={post.postId} post={post} />
+          ))}
+        </Fragment>
+      ))}
+      <div ref={ref} style={{ height: 50 }} />
+    </>
+  );
 }
