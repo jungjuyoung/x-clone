a6c6014120f5b6a01bbb9c5047bc3b1ea3ece8a1
jungjuyoung <nwwn1004@naver.com>
2024-02-19 02:18:05 +0900

    feat: 트랜드 mock데이터 수정, Trend 컴포넌트 수정

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Trend({ trend }: Props) {
   return (
     <Link href={`/search?q=${trend.title}`} className={style.container}>
-      <div className={style.count}>{trend.count}</div>
+      <div className={style.count}>{trend.tagId}</div>
       <div className={style.title}>{trend.title}</div>
       <div className={style.count}>{trend.count.toLocaleString()} posts</div>
     </Link>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -316,15 +316,15 @@ export const handlers = [
   http.get('/api/trends', ({ request }) => {
     return HttpResponse.json(
       [
-        { tagId: 1, title: '제로초', count: 1264 },
-        { tagId: 2, title: '원초', count: 1264 },
+        { tagId: 1, title: '제로초', count: 823601 },
+        { tagId: 2, title: '원초', count: 385 },
         { tagId: 3, title: '투초', count: 1264 },
-        { tagId: 4, title: '쓰리초', count: 1264 },
-        { tagId: 5, title: '포초', count: 1264 },
-        { tagId: 6, title: '파이브초', count: 1264 },
-        { tagId: 7, title: '식스초', count: 1264 },
-        { tagId: 8, title: '세븐초', count: 1264 },
-        { tagId: 9, title: '나인초', count: 1264 },
+        { tagId: 4, title: '쓰리초', count: 3371 },
+        { tagId: 5, title: '포초', count: 627 },
+        { tagId: 6, title: '파이브초', count: 8 },
+        { tagId: 7, title: '식스초', count: 2889 },
+        { tagId: 8, title: '세븐초', count: 33382 },
+        { tagId: 9, title: '나인초', count: 9 },
       ]
     )
   }),
