edcea70ee9c14a7e86a11ca73306efd2e92650a0
juyoung <nwwn1004@naver.com>
2024-02-22 05:58:20 +0900

    feat: next config rewrites 추가

--- next.config.js
+++ next.config.js
@@ -5,7 +5,15 @@ const nextConfig = {
             { protocol: "https", hostname: "**.domain.co(m|\.kr)" },
             { protocol: "http", hostname: "**.domain.co(m|\.kr)" },
         ]
-    }
+    },
+    async rewrites() {
+        return [
+          {
+            source: '/upload/:slug',
+            destination: 'http://localhost:9090/upload/:slug',
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
