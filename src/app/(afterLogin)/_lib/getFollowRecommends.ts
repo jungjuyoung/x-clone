b642fc3a9e6eab1a3a02e2647d79f46a53f00de5
juyoung <nwwn1004@naver.com>
2024-03-01 03:21:49 +0900

    feat: AUTH_URL 제거 NEXT_PUBLIC_BASE_URL로 대체

--- src/app/(afterLogin)/_lib/getFollowRecommends.ts
+++ src/app/(afterLogin)/_lib/getFollowRecommends.ts
@@ -1,5 +1,5 @@
 export async function getFollowRecommends() {
-  const res = await fetch(`${process.env.AUTH_URL}/api/followRecommends`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/followRecommends`, {
     next: {
       tags: ['users', 'followRecommends'],
     },
