69a81f93cd3ce087bc1da5524a363167fe99f03d
juyoung <nwwn1004@naver.com>
2024-03-08 03:16:18 +0900

    feat: 트렌드 credentials설정, Backend에서 전달된 api 로 라우터 변경

--- src/app/(afterLogin)/_lib/getTrends.ts
+++ src/app/(afterLogin)/_lib/getTrends.ts
@@ -1,9 +1,10 @@
 type Props = { pageParam?: number };
 export async function getTrends({ pageParam }: Props) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/trends`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hashtags/trends`, {
     next: {
       tags: ['trends'],
     },
+    credentials:'include'
   });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
