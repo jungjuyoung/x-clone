1a824e7dd974fc2693337957712a0e5613c006c6
juyoung <nwwn1004@naver.com>
2024-03-08 03:30:19 +0900

    feat: 주소창에 querystring에 해시태그 특수기호 입력되는 현상 encodeURIComponent적용

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 export default function Trend({ trend }: Props) {
   return (
-    <Link href={`/search?q=${trend.title}`} className={style.container}>
+    <Link href={`/search?q=${encodeURIComponent(trend.title)}`} className={style.container}>
       <div className={style.count}>{trend.tagId}</div>
       <div className={style.title}>{trend.title}</div>
       <div className={style.count}>{trend.count.toLocaleString()} posts</div>
