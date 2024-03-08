0febd4069283963e6655b74ef0933ded3e3164cc
juyoung <nwwn1004@naver.com>
2024-03-08 13:47:02 +0900

    feat: Backbutton 컴포넌트 기능 적용

--- src/app/(afterLogin)/[username]/_component/UserInfo.tsx
+++ src/app/(afterLogin)/[username]/_component/UserInfo.tsx
@@ -21,8 +21,6 @@ export default function UserInfo({ username }: Props) {
     staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
     gcTime: 300 * 1000,
   });
-  console.log("error");
-  console.dir(error);
   if (error) {
     return (
       <>
