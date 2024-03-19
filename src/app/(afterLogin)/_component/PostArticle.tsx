6e80be324fac0ed56a21601e4ab703f2ec5b32c5
juyoung <nwwn1004@naver.com>
2024-03-20 01:54:58 +0900

    feat: 답글, 재개시글

--- src/app/(afterLogin)/_component/PostArticle.tsx
+++ src/app/(afterLogin)/_component/PostArticle.tsx
@@ -1,30 +1,24 @@
 "use client";
 
-import { ReactNode } from "react";
-import style from "./post.module.css";
-import { useRouter } from "next/navigation";
+import {EventHandler, MouseEventHandler, ReactNode} from "react";
+import style from './post.module.css';
+import {useRouter} from "next/navigation";
+import {Post} from "@/model/Post";
 
 type Props = {
-  children: ReactNode;
-  post: {
-    postId: number;
-    content: string;
-    User: {
-      id: string;
-      nickname: string;
-      image: string;
-    };
-    createdAt: Date;
-    Images: any[];
-  };
-};
+  children: ReactNode,
+  post: Post
+}
 
-export default function PostArticle({ children, post }: Props) {
+export default function PostArticle({ children, post}: Props) {
   const router = useRouter();
+  let target = post;
+  if (post.Original) {
+    target = post.Original;
+  }
   const onClick = () => {
-    // console.log("PostArticle onClickCapture router.push call");
-    router.push(`/${post.User.id}/status/${post.postId}`);
-  };
+    router.push(`/${target.User.id}/status/${target.postId}`);
+  }
 
   return (
     <article onClick={onClick} className={style.post}>
