51bd3498ae22d56a34615089ce1be2ccf1186995
juyoung <nwwn1004@naver.com>
2024-03-22 01:07:57 +0900

    fix: 대소문자 오타 수정

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -6,13 +6,13 @@ import Trend from "./Trend";
 import style from "./trendSection.module.css";
 import { useSession } from "next-auth/react";
 import { getTrends } from "../_lib/getTrends";
-import { HashTag } from "@/model/HashTag";
+import { Hashtag } from "@/model/HashTag";
 
 export default function TrendSection() {
   const pathname = usePathname();
   const { data: session } = useSession();
 
-  const { data } = useQuery<HashTag[]>({
+  const { data } = useQuery<Hashtag[]>({
     queryKey: ["trends"],
     queryFn: getTrends,
     staleTime: 60 * 1000, // fresh -> stale, 기본값이 0. 5분이라는 기준
@@ -27,7 +27,7 @@ export default function TrendSection() {
         <div className={style.trend}>
           <h3>나를 위한 트랜드</h3>
           {/* Trend 서버에서 받아와서 돌림 */}
-          {data?.map<HashTag>((trend) => (
+          {data?.map<Hashtag>((trend) => (
             <Trend key={trend.tagId} trend={trend} />
           ))}
         </div>
