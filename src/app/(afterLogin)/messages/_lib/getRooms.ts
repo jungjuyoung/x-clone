5ef943a86f68a8a7fc8826cfc46a4f57c83bd8d9
juyoung <nwwn1004@naver.com>
2024-03-23 02:52:54 +0900

    feat: Room페이지 채팅목록 구현

---
+++ src/app/(afterLogin)/messages/_lib/getRooms.ts
@@ -0,0 +1,18 @@
+import {cookies} from "next/headers";
+import {Room} from "@/model/Room";
+
+export const getRooms = async (id: string) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${id}/rooms`, {
+    next: {
+      tags: ['rooms']
+    },
+    credentials: 'include',
+    headers: {Cookie: cookies().toString()},
+    cache: 'no-store'
+  });
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+  return res.json() as Promise<Room[]>
+}
