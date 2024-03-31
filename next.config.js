6b5c89a3b03b05cdd7b6cd64dbbd2e464d69ccf3
juyoung <nwwn1004@naver.com>
2024-03-31 13:46:10 +0900

    feat: vanilla-extract

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
+const {
+  createVanillaExtractPlugin
+} = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            { protocol: "https", hostname: "**.domain.co(m|\.kr)" },
-            { protocol: "http", hostname: "**.domain.co(m|\.kr)" },
-        ]
-    },
-    async rewrites() {
-        return [
-          {
-            source: '/upload/:slug',
-            destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`
-          },
-        ]
+  async rewrites() {
+    return [
+      {
+        source: '/upload/:slug',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:slug`
       },
+    ]
+  },
 }
 
-module.exports = nextConfig
+module.exports = withVanillaExtract(nextConfig);
